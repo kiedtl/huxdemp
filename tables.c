98f194b02b5606f4fe50b4827ffd324a91bf3c0d
Kiëd Llaentenn <kiedtl@tilde.team>
2021-11-24 22:04:25 +0000

    fix: Out of range when looking for char in t_cntrls
    This fix was suggested by @skeeto a while back. I thought I had applied
    the fix, but apparently I didn't...
    
    - https://old.reddit.com/r/commandline/comments/ldod73/a_colorized_alternative_to_hexdump/gm8bytx/

--- tables.c
+++ tables.c
@@ -1,6 +1,6 @@
 /* Character for space was ␠, changed back to ' ' for
  * readability */
-static char *t_cntrls[] = {
+static char *t_cntrls[256] = {
 	[0  ] = "␀", [1  ] = "␁", [2  ] = "␂", [3  ] = "␃",
 	[4  ] = "␄", [5  ] = "␅", [6  ] = "␆", [7  ] = "␇",
 	[8  ] = "␈", [9  ] = "␉", [10 ] = "␊", [11 ] = "␋",
