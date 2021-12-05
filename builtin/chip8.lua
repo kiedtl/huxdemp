9ef6a59ed49effbbf5667433588a7a7dca68ddf9
Kiëd Llaentenn <kiedtl@tilde.team>
2021-12-05 21:08:55 +0000

    feat: add coloring to chip8 plugin

--- builtin/chip8.lua
+++ builtin/chip8.lua
@@ -4,8 +4,17 @@
 
 local M = {}
 
-pbuffer = nil
-skip_next_op = false
+local styles = {
+    chip8 = "",
+    schip = "\x1b[38;5;6m",
+    xochip = "\x1b[38;5;5m",
+    continued = "\x1b[38;5;7m",
+    unknown = "\x1b[38;5;8m",
+    skipped = "\x1b[38;5;7m",
+}
+
+local pbuffer = nil
+local skip_next_op = false
 
 local function decode(buffer, index)
     local op = buffer[index] << 8 | buffer[index + 1]
@@ -15,21 +24,21 @@ local function decode(buffer, index)
     local N      = (op >>  0) & 0xF;
     local NN     = (op >>  0) & 0xFF;
 
-    local opstring = ({
+    local opstring, optype = ({
         [0x0] = function ()
             local _switch = {
-                [0xC] = function () return "00CN" end,
-                [0xD] = function () return "00DN" end,
+                [0xC] = function () return "00CN", "schip" end,
+                [0xD] = function () return "00DN", "xochip" end,
                 ["d"] = function ()
                     local _switch = {
                         [0x00E0] = function () return "00E0" end,
                         [0x00EE] = function () return "00EE" end,
-                        [0x00FB] = function () return "00FB" end,
-                        [0x00FC] = function () return "00FC" end,
-                        [0x00FD] = function () return "00FD" end,
-                        [0x00FE] = function () return "00FE" end,
-                        [0x00FF] = function () return "00FF" end,
-                        ["d"] = function () return "????" end,
+                        [0x00FB] = function () return "00FB", "schip" end,
+                        [0x00FC] = function () return "00FC", "schip" end,
+                        [0x00FD] = function () return "00FD", "schip" end,
+                        [0x00FE] = function () return "00FE", "schip" end,
+                        [0x00FF] = function () return "00FF", "schip" end,
+                        ["d"] = function () return "????", "unknown" end,
                     }
                     return (_switch[op] or _switch["d"])()
                 end
@@ -43,9 +52,9 @@ local function decode(buffer, index)
         [0x5] = function()
             local _switch = {
                 [0x0] = function () return "5XY0" end,
-                [0x2] = function () return "5XY2" end,
-                [0x3] = function () return "5XY3" end,
-                ["d"] = function () return "????" end,
+                [0x2] = function () return "5XY2", "xochip" end,
+                [0x3] = function () return "5XY3", "xochip" end,
+                ["d"] = function () return "????", "unknown" end,
             }
             return (_switch[N] or _switch["d"])()
         end,
@@ -62,7 +71,7 @@ local function decode(buffer, index)
                 [0x6] = function () return "8X06" end,
                 [0x7] = function () return "8XY7" end,
                 [0xE] = function () return "8X0E" end,
-                ["d"] = function () return "????" end,
+                ["d"] = function () return "????", "unknown" end,
             }
             return (_switch[N] or _switch["d"])()
         end,
@@ -70,44 +79,55 @@ local function decode(buffer, index)
         [0xA] = function() return "ANNN" end,
         [0xB] = function() return "BNNN" end,
         [0xC] = function() return "CXNN" end,
-        [0xD] = function() return "DXYN" end,
+        [0xD] = function()
+            if N == 0 then return "DXY0", "schip" end
+            return "DXYN"
+        end,
         [0xE] = function()
             local _switch = {
                 [0x9E] = function () return "EX9E" end,
                 [0xA1] = function () return "EXA1" end,
-                ["d"] = function () return "????" end,
+                ["d"] = function () return "????", "unknown" end,
             }
             return (_switch[NN] or _switch["d"])()
         end,
         [0xF] = function()
             local _switch = {
                 [0x00] = function ()
-                    if op == 0xF000 then return "F000" end
-                    return "????"
+                    if op == 0xF000 then return "F000", "xochip" end
+                    return "????", "unknown"
                 end,
-                [0x01] = function () return "FX01" end,
-                [0x02] = function () return "FX02" end,
+                [0x01] = function () return "FX01", "xochip" end,
+                [0x02] = function () return "F002", "xochip" end,
                 [0x07] = function () return "FX07" end,
                 [0x15] = function () return "FX15" end,
                 [0x18] = function () return "FX18" end,
                 [0x29] = function () return "FX29" end,
-                [0x30] = function () return "FX30" end,
+                [0x30] = function () return "FX30", "schip" end,
                 [0x1E] = function () return "FX1E" end,
                 [0x0A] = function () return "FX0A" end,
                 [0x33] = function () return "FX33" end,
+                [0x33] = function () return "FX3A", "xochip" end,
                 [0x55] = function () return "FX55" end,
                 [0x65] = function () return "FX65" end,
-                [0x75] = function () return "FX75" end,
-                [0x85] = function () return "FX85" end,
-                ["d"] = function () return "????" end,
+                [0x75] = function () return "FX75", "schip" end,
+                [0x85] = function () return "FX85", "schip" end,
+                ["d"] = function () return "????", "unknown" end,
             }
             return (_switch[NN] or _switch["d"])()
         end,
     })[P]()
-    return opstring
+    return opstring, optype or "chip8"
 end
 
 function M.main(buffer, offset, use_color, out)
+    local function s(style, text)
+        if use_color then
+            return style .. text .. "\x1b[m"
+        end
+        return ""
+    end
+
     local i = 1
 
     if pbuffer then
@@ -118,7 +138,7 @@ function M.main(buffer, offset, use_color, out)
     while i <= #buffer do
         if skip_next_op then
             skip_next_op = false
-            out:write("---- ")
+            out:write(s(styles.skipped, "**** "))
             i = i + 2
             goto continue
         end
@@ -126,12 +146,12 @@ function M.main(buffer, offset, use_color, out)
         -- Buffer input if we don't have enough bytes.
         if (#buffer + 1) - i < 2 then
             pbuffer = {table.unpack(buffer, i, #buffer)}
-            out:write("---- ")
+            out:write(s(styles.continued, "---- "))
             break
         end
 
-        local opstring = decode(buffer, i)
-        out:write(opstring .. " ")
+        local opstring, optype = decode(buffer, i)
+        out:write(s(styles[optype], opstring .. " "))
 
         if opstring == "F000" then
             skip_next_op = true
